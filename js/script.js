d153980a99cc1f5ce88b92ff5b70bb105132c0ae
renlin-code <renelj1997@gmail.com>
2022-03-26 20:55:55 +0500

    Menu fixed listo

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
 //NAVIGATOR MENU
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY >= 20) {
+        header.classList.add("header_active");
+    }
+    else {
+        header.classList.remove("header_active");
+    }
+})
 const menuButtom = document.querySelector(".header-nav--menu-button");
 const navMenu = document.querySelector(".nav-menu");
-const body = document.querySelector("body");
-var menuItem = document.querySelector(".menu-item");
 
 menuButtom.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
-    body.classList.toggle("body_fixed");
 });
-menuItem.addEventListener("click", () => {
+function menuItemClick(){
     navMenu.classList.remove("nav-menu_visible");
-    body.classList.remove("body_fixed");
-});
+};
+
+
+    if (document.getElementsByClassName("nav-menu_visible") == navMenu)
+    {
+        header.classList.add("header_active");
+    }
+    else {
+        header.classList.remove("header_active");
+    }
 
 
 //CERTIFICATES
